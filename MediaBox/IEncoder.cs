f04b1dd2b781acd313a4888a926a5d1e77bdb957
Timberfang <timberfang.code@pm.me>
2025-07-06 14:34:01 -0500

    feat: Overhaul video encoder

--- MediaBox/IEncoder.cs
+++ MediaBox/IEncoder.cs
@@ -2,7 +2,11 @@ namespace MediaBox;
 
 public interface IEncoder
 {
-	string[] GetArgs(string inPath);
-	void Encode(string inPath, string outPath);
-	void EncodeDirectory(string inPath, string outPath);
+	string InPath { get; set; }
+	string OutPath { get; set; }
+	EncoderPreset Preset { get; set; }
+
+	void Encode();
+	void Encode(string path);
+	void EncodeDirectory();
 }
--- MediaBox/VideoEncoder.cs
+++ MediaBox/VideoEncoder.cs
@@ -1,52 +1,139 @@
 using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace MediaBox;
 
-public class VideoEncoder : IEncoder
+public partial class VideoEncoder(string inPath, string outPath, EncoderPreset preset = EncoderPreset.Normal)
+	: IEncoder
 {
 	private const string VideoCodec = "libsvtav1";
 	private const string AudioCodec = "libopus";
-	private readonly int _audioBitrate;
 
-	private readonly string[] _filter =
-		[".mkv", ".webm", ".mp4", ".m4v", ".m4a", ".avi", ".mov", ".qt", ".ogv", ".gif"];
+	private readonly Dictionary<EncoderPreset, int> _audioBitrate =
+		new() { { EncoderPreset.Quality, 128000 }, { EncoderPreset.Normal, 96000 } };
 
-	private readonly int _videoPreset;
-	private readonly int _videoQuality;
+	private readonly string[] _filter = [".mkv", ".webm", ".mp4", ".m4v", ".m4a", ".avi", ".mov", ".qt", ".ogv"];
 
-	public VideoEncoder(EncoderPreset preset = EncoderPreset.Normal)
+	private readonly Dictionary<EncoderPreset, int> _videoPreset =
+		new() { { EncoderPreset.Quality, 6 }, { EncoderPreset.Normal, 10 } };
+
+	private readonly Dictionary<EncoderPreset, int> _videoQuality =
+		new() { { EncoderPreset.Quality, 27 }, { EncoderPreset.Normal, 33 } };
+
+	public int VideoPreset => _videoPreset[Preset];
+
+	public int VideoQuality => _videoQuality[Preset];
+
+	public int AudioBitrate => _audioBitrate[Preset];
+
+	public string InPath { get; set; } = inPath;
+	public string OutPath { get; set; } = outPath;
+	public EncoderPreset Preset { get; set; } = preset;
+	public void Encode() => Encode(false, false);
+
+	public void Encode(string path) => Encode(path, false, false);
+
+	// ReSharper disable once MemberCanBePrivate.Global
+	public void Encode(bool noSurround, bool noCrop) => Encode(InPath, noSurround, noCrop);
+
+	// ReSharper disable once MemberCanBePrivate.Global
+	public void Encode(string path, bool noSurround, bool noCrop)
 	{
-		switch (preset)
+		string target = GetTargetPath(path);
+		string targetParent = Directory.GetParent(target)?.FullName ?? throw new InvalidOperationException();
+		if (Path.Exists(target))
 		{
-			case EncoderPreset.Quality:
-				_videoQuality = 27;
-				_videoPreset = 6;
-				_audioBitrate = 128000;
-				break;
-			case EncoderPreset.Normal:
-			default:
-				_videoQuality = 33;
-				_videoPreset = 10;
-				_audioBitrate = 96000;
-				break;
+			return;
+		}
+
+		if (!Directory.Exists(targetParent))
+		{
+			Directory.CreateDirectory(targetParent);
+		}
+
+		using Process ffmpeg = new();
+		ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+		ffmpeg.StartInfo.Arguments =
+			$"-i \"{path}\" {string.Join(" ", GetArgs(path, noSurround, noCrop))} \"{target}\" -nostdin";
+		ffmpeg.Start();
+		ffmpeg.WaitForExit();
+
+		// ReSharper disable once InvertIf
+		if (ffmpeg.ExitCode != 0)
+		{
+			if (File.Exists(target))
+			{
+				File.Delete(target);
+			}
+
+			throw new InvalidOperationException("ffmpeg crashed, deleting target file");
 		}
 	}
 
-	public string[] GetArgs(string inPath)
+	public void EncodeDirectory() => EncodeDirectory(false, false);
+
+	// ReSharper disable once MemberCanBePrivate.Global
+	public void EncodeDirectory(bool noSurround, bool noCrop)
 	{
-		int channels = GetChannelCount(inPath);
+		IEnumerable<string> files = Directory.EnumerateFiles(InPath, "*", SearchOption.AllDirectories)
+			.Where(f => _filter.Contains(Path.GetExtension(f)));
+		foreach (string file in files)
+		{
+			Encode(file, noSurround, noCrop);
+		}
+	}
+
+	private static int GetChannelCount(string path)
+	{
+		using Process ffprobe = new();
+		ffprobe.StartInfo.FileName = "ffprobe.exe";
+		ffprobe.StartInfo.Arguments =
+			$"-select_streams a:0 -show_entries stream=channels -of compact=p=0:nk=1 -loglevel error \"{path}\"";
+		ffprobe.StartInfo.RedirectStandardOutput = true;
+		ffprobe.Start();
+		string output = ffprobe.StandardOutput.ReadToEnd();
+		ffprobe.WaitForExit();
+		if (ffprobe.ExitCode != 0)
+		{
+			throw new InvalidOperationException(output);
+		}
+
+		return int.TryParse(output, out int channels) ? channels : 0;
+	}
+
+	private static string GetCroppingConfig(string path)
+	{
+		using Process ffmpeg = new();
+		ffmpeg.StartInfo.FileName = "ffmpeg.exe";
+		ffmpeg.StartInfo.Arguments =
+			$"-skip_frame nokey -y -hide_banner -nostats -t 10:00 -i \"{path}\" -vf cropdetect -an -f null -";
+		ffmpeg.StartInfo.RedirectStandardError = true;
+		ffmpeg.Start();
+		string output = ffmpeg.StandardError.ReadToEnd();
+		ffmpeg.WaitForExit();
+		if (ffmpeg.ExitCode != 0)
+		{
+			throw new InvalidOperationException(output);
+		}
+
+		return CroppingRegex().Match(output).Groups[0].Value;
+	}
+
+	private string[] GetArgs(string path, bool noSurround, bool noCrop)
+	{
+		int channels = noSurround ? 2 : GetChannelCount(path);
 		int targetAudioBitrate = channels switch
 		{
-			>= 7 => Convert.ToInt32(_audioBitrate * 2.5),
-			>= 5 => Convert.ToInt32(_audioBitrate) * 2,
-			_ => _audioBitrate
+			>= 7 => Convert.ToInt32(AudioBitrate * 2.5),
+			>= 5 => Convert.ToInt32(AudioBitrate) * 2,
+			_ => AudioBitrate
 		};
 
-		string[] args =
+		List<string> args =
 		[
 			"-c:v", VideoCodec,
-			"-crf", _videoQuality.ToString(),
-			"-preset", _videoPreset.ToString(),
+			"-crf", VideoQuality.ToString(),
+			"-preset", VideoPreset.ToString(),
 			"-c:a", AudioCodec,
 			"-b:a", targetAudioBitrate.ToString(),
 			"-c:s", "copy",
@@ -54,39 +141,25 @@ public class VideoEncoder : IEncoder
 			"aformat=channel_layouts=7.1|5.1|stereo" // Workaround for a bug with opus in ffmpeg, see https://trac.ffmpeg.org/ticket/5718
 		];
 
-		return args;
-	}
-
-	public void EncodeDirectory(string inPath, string outPath)
-	{
-		IEnumerable<string> files = Directory.EnumerateFiles(inPath, "*", SearchOption.AllDirectories)
-			.Where(f => _filter.Contains(Path.GetExtension(f)));
-		foreach (string file in files)
+		// ReSharper disable once InvertIf
+		if (!noCrop)
 		{
-			string target = Path.Combine(outPath, Path.GetFileNameWithoutExtension(file) + ".mkv");
-			Encode(file, target);
+			string croppingConfig = GetCroppingConfig(path);
+			// ReSharper disable once InvertIf
+			if (!string.IsNullOrEmpty(croppingConfig))
+			{
+				args.Add("-vf");
+				args.Add(croppingConfig);
+			}
 		}
+
+		return args.ToArray();
 	}
 
-	public void Encode(string inPath, string outPath)
-	{
-		using Process ffmpeg = new();
-		ffmpeg.StartInfo.FileName = "ffmpeg.exe";
-		ffmpeg.StartInfo.Arguments = $"-i \"{inPath}\" {string.Join(" ", GetArgs(inPath))} \"{outPath}\" -nostdin";
-		ffmpeg.Start();
-		ffmpeg.WaitForExit();
-	}
+	private string GetTargetPath(string path) => Path.GetExtension(OutPath).Length == 0
+		? Path.Join(OutPath, path.Replace(InPath, string.Empty))
+		: Path.ChangeExtension(OutPath, ".mkv");
 
-	private static int GetChannelCount(string inPath)
-	{
-		using Process ffprobe = new();
-		ffprobe.StartInfo.FileName = "ffprobe.exe";
-		ffprobe.StartInfo.Arguments =
-			$"-select_streams a:0 -show_entries stream=channels -of compact=p=0:nk=1 -loglevel error \"{inPath}\"";
-		ffprobe.StartInfo.RedirectStandardOutput = true;
-		ffprobe.Start();
-		ffprobe.WaitForExit();
-		int output = int.Parse(ffprobe.StandardOutput.ReadToEnd());
-		return output;
-	}
+	[GeneratedRegex("crop=.*", RegexOptions.RightToLeft)]
+	private static partial Regex CroppingRegex();
 }
