cd1ea17e44ceaa1ace583b3359dac70a663909d2
Timberfang <timberfang.code@pm.me>
2025-07-08 11:39:19 -0500

    feat: Add basic CLI

--- MediaBox/Program.cs
+++ MediaBox/Program.cs
@@ -1,10 +1,8 @@
+using MediaBox.Utilities;
+
 namespace MediaBox;
 
 internal static class Program
 {
-	private static void Main(string[] args)
-	{
-		// TODO: Implement CLI using https://github.com/Cysharp/ConsoleAppFramework
-		Console.WriteLine("Hello World!");
-	}
+	private static void Main(string[] args) => CommandLine.StartCommandline(args);
 }
---
+++ MediaBox/Utilities/CommandLine.cs
@@ -0,0 +1,67 @@
+using ConsoleAppFramework;
+using MediaBox.Encoding;
+
+namespace MediaBox.Utilities;
+
+public static class CommandLine
+{
+	private static readonly Dictionary<string, EncoderPreset> s_encoderPresets =
+		new() { { "quality", EncoderPreset.Quality }, { "normal", EncoderPreset.Normal } };
+	private static readonly string[] s_allowedTypes = ["video"];
+	private static readonly string[] s_allowedPresets = ["quality", "normal"];
+
+	public static void StartCommandline(string[] args)
+	{
+		ConsoleApp.ConsoleAppBuilder app = ConsoleApp.Create();
+		app.Add("transcode", Commands.Transcode);
+		app.Run(args);
+	}
+
+	private static class Commands
+	{
+		/// <summary>
+		///     Transcode media to a different format.
+		/// </summary>
+		/// <param name="type">-t, Type of media. The only allowed value is "video".</param>
+		/// <param name="path">-p, Path to the media file or directory.</param>
+		/// <param name="destination">-d, Path where the transcoded media will be saved.</param>
+		/// <param name="preset">Preset for the media. Allowed values are "quality" and "normal".</param>
+		public static async Task Transcode(
+			string type,
+			string path,
+			string destination,
+			string preset = "normal")
+		{
+			// [AllowedValues()] Could be used here, but it's not compatible with AOT.
+			if (!s_allowedTypes.Contains(type))
+			{
+				Console.WriteLine("The type must be one of: 'video'.");
+				return;
+			}
+			if (!s_allowedPresets.Contains(preset))
+			{
+				Console.WriteLine("The preset must be one of: 'quality', 'normal'.");
+				return;
+			}
+			if (!Directory.Exists(path) && !File.Exists(path))
+			{
+				Console.WriteLine("Invalid path: " + path);
+				return;
+			}
+
+			// It's okay if the destination exists as a directory; the encoder will place files inside the directory.
+			if (File.Exists(destination))
+			{
+				Console.WriteLine("Destination already exists: " + destination);
+				return;
+			}
+			switch (type)
+			{
+				case "video":
+					VideoEncoder encoder = new(path, destination, s_encoderPresets[preset.ToLowerInvariant()]);
+					await encoder.EncodeAsync();
+					break;
+			}
+		}
+	}
+}
