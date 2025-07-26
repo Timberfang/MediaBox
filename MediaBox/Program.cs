c5b6b22a61c2e81cbd4153f725b4554b707024de
Timberfang <timberfang.code@pm.me>
2025-07-25 21:46:43 -0500

    chore: remove outdated comment

--- MediaBox/Program.cs
+++ MediaBox/Program.cs
@@ -4,7 +4,6 @@ namespace MediaBox;
 
 internal static class Program
 {
-	// TODO: Allow extension/codec(s) to be changed
 	private static void Main(string[] args)
 	{
 		CommandLine.StartCommandline(args);
